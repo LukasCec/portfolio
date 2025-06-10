1ee392bbeae569a1fbdab445c60738b63a6229c2
Lukáš Čeč <lukas.cec@student.tuke.sk>
2025-06-10 13:18:52 +0200

    responsivity issues fix in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-// layout.tsx
 'use client'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -8,10 +7,6 @@ import CornerClock from "@/app/components/CornerClock";
 import ConsoleTexts from "@/app/components/ConsoleTexts";
 import { GeistSans } from "geist/font/sans";
 import React from "react";
-
-
-
-
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -26,18 +21,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en" className={GeistSans.className}>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black min-h-screen relative`}>
-
         <div className="fixed inset-0 flex items-center justify-center bg-black">
-            <CornerClock />
-            <ConsoleTexts />
+            {/* Skry na mobile: */}
+            <div className="hidden sm:block"><CornerClock /></div>
+            <div className="hidden sm:block"><ConsoleTexts /></div>
             <div className="fixed top-4 left-4 right-4 bottom-4 lg:top-10 lg:left-10 lg:right-10 lg:bottom-10 border border-white/60 overflow-hidden shadow-xl">
                 <VantaBackground />
-
-
                 <div className="relative z-10 h-full w-full grid
-                        grid-cols-1
-                        lg:grid-cols-[260px_1fr_760px]
-                        grid-rows-[1fr_auto]">
+                            grid-cols-1
+                            lg:grid-cols-[260px_1fr_760px]
+                            grid-rows-[1fr_auto]">
                     <LeftSection />
                     {children}
                 </div>
